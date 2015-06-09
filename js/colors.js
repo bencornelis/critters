963f3c10b846fad0c6d867da80588bf8f8b25c61
Ben Cornelis <bencornelis02@gmail.com>
2015-06-08 19:14:27 -0700

    get basic animation working

---
+++ js/colors.js
@@ -0,0 +1,290 @@
+Array.prototype.delete = function(element) {
+  var i = this.indexOf(element);
+  this.splice(i, 1);
+}
+
+var shuffle = function(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i]
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+}
+
+Color.prototype.distance = function(otherColor) {
+  var dr = this.r - otherColor.r;
+  var dg = this.g - otherColor.g;
+  var db = this.b - otherColor.b;
+  return Math.pow(dr, 2) + Math.pow(dg, 2) + Math.pow(db, 2);
+}
+
+Color.prototype.toString = function() {
+  return "rgb(" + this.r + "," + this.g + "," + this.b + ")";
+}
+
+function Cell(x, y) {
+  this.x = x;
+  this.y = y;
+  this.color = Color(0, 0, 0);
+  this.empty = true;
+}
+
+Cell.prototype.changeColor = function(newColor) {
+  this.color = newColor;
+  this.empty = false;
+}
+
+Cell.prototype.toString = function() {
+  return this.x + "-" + this.y;
+}
+
+function Grid(height, width) {
+  this.grid = [];
+  this.boundary = [];
+  this.height = height;
+  this.width = width;
+}
+
+Grid.prototype.populate = function() {
+  for (var y = 0; y < this.height; y++) {
+    var row = [];
+    for (var x = 0; x < this.width; x++) {
+      row.push(new Cell(x, y));
+    }
+    this.grid.push(row);
+  }
+}
+
+Grid.prototype.getCell = function(x, y) {
+  return this.grid[y][x];
+}
+
+Grid.prototype.cells = function() {
+  var cells = [];
+  this.grid.forEach(function(row) { cells += row; });
+  return cells;
+}
+
+Grid.prototype.inGrid = function(x, y) {
+  var xInGrid = x >= 0 && x < this.width,
+      yInGrid = y >= 0 && y < this.height;
+  return xInGrid && yInGrid;
+}
+
+Grid.prototype.getNeighbors = function(cell, isEmpty) {
+  var x = cell.x;
+      y = cell.y;
+  var neighbors = [];
+  for (var j = y - 1; j <= y + 1; j++) {
+    for (var i = x - 1; i <= x + 1; i++) {
+      if (this.inGrid(i,j) && this.getCell(i,j) != cell && this.getCell(i, j).empty === isEmpty) {
+        neighbors.push(this.getCell(i, j));
+      }
+    }
+  }
+  return neighbors;
+}
+
+Grid.prototype.emptyNeighbors = function(cell) {
+  return this.getNeighbors(cell, true);
+}
+
+Grid.prototype.occupiedNeighbors = function(cell) {
+  return this.getNeighbors(cell, false);
+}
+
+Grid.prototype.hasNeighbors = function(cell, isEmpty) {
+  var x = cell.x,
+      y = cell.y;
+  for (var j = y - 1; j <= y + 1; j++) {
+    for (var i = x - 1; i <= x + 1; i++) {
+      if (this.inGrid(i, j) && this.getCell(i, j).empty === isEmpty) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+Grid.prototype.hasEmptyNeighbors = function(cell) {
+  return this.hasNeighbors(cell, true);
+}
+
+Grid.prototype.hasOccupiedNeighbors = function(cell) {
+  return this.hasNeighbors(cell, false);
+}
+
+Grid.prototype.boundary = function() {
+  var boundary = [];
+  this.cells().forEach(function(cell) {
+    if (cell.empty && this.hasOccupiedNeighbors(cell)) {
+      boundary.push(cell);
+    }
+  });
+  return boundary;
+}
+
+Grid.prototype.updateBoundary = function(newCell) {
+
+  this.boundary.delete(newCell);
+
+  //add new boundary cells
+  var emptyNeighbors = this.emptyNeighbors(newCell)
+  for (var k = 0; k < emptyNeighbors.length; k++) {
+    var cell = emptyNeighbors[k];
+    var newBoundary = true,
+        x           = cell.x,
+        y           = cell.y;
+    for (var j = y - 1; j <= y + 1; j++) {
+      for (var i = x - 1; i <= x + 1; i++) {
+        if (this.inGrid(i, j)) {
+          var neighbor = this.getCell(i, j);
+          if (!neighbor.empty && neighbor != newCell) {
+            newBoundary = false;
+            j = y + 2; i = x + 2;
+          }
+        }
+      }
+    }
+    if (newBoundary) { this.boundary.push(cell); }
+  }
+}
+
+function ColorMapper(height, width) {
+  this.grid = new Grid(height, width);
+  this.palette = [];
+  this.n = 32;
+  this.L = parseInt(256/this.n);
+  this.coloredCells = [];
+}
+
+ColorMapper.prototype.setUp = function() {
+  this.generatePalette();
+  this.grid.populate();
+}
+
+ColorMapper.prototype.generatePalette = function() {
+  var orderedColors = [];
+  for (var i = 0; i < this.n; i++) {
+    for (var j = 0; j < this.n; j++) {
+      for (var k = 0; k < this.n; k++) {
+        orderedColors.push(new Color(i*this.L, j*this.L, k*this.L))
+      }
+    }
+  }
+  this.palette = shuffle(orderedColors);
+}
+
+ColorMapper.prototype.chooseColor = function() {
+  return this.palette.splice(0, 1)[0];
+}
+
+ColorMapper.prototype.averageColorDist = function(color, neighborCells) {
+  var numCells = neighborCells.length;
+  var distSum = 0;
+  neighborCells.forEach(function(neighborCell) { distSum += color.distance(neighborCell.color); });
+  return distSum/numCells;
+}
+
+ColorMapper.prototype.colorCell = function(cell, color) {
+  cell.changeColor(color);
+}
+
+ColorMapper.prototype.bestCell = function(color) {
+  var boundary = this.grid.boundary;
+  var bestCell = boundary[0],
+      minDist  = this.averageColorDist(color, this.grid.occupiedNeighbors(boundary[0]));
+
+  for (var i = 0; i < boundary.length; i++) {
+    var cell = boundary[i];
+    var dist = this.averageColorDist(color, this.grid.occupiedNeighbors(cell));
+    if (dist < minDist) {
+      minDist = dist;
+      bestCell = cell;
+    }
+  }
+
+  return bestCell;
+}
+
+ColorMapper.prototype.colorNextCell = function() {
+  var color = this.chooseColor()
+  var bestCell = this.bestCell(color)
+  this.colorCell(bestCell, color);
+  // bestCell.paint();
+  this.coloredCells.push(bestCell);
+  this.grid.updateBoundary(bestCell);
+}
+
+
+Cell.prototype.paint = function() {
+  $("#" + this.toString()).css("background-color", this.color.toString());
+}
+
+$(function() {
+
+  //setup
+  $("#painting").html(generateGrid(128, 64));
+
+  var paintDisShit  = function() {
+    var artist = new ColorMapper(128, 64);
+    artist.setUp();
+    var startCell = artist.grid.getCell(32, 64);
+    artist.colorCell(startCell, artist.chooseColor());
+    startCell.paint();
+    artist.grid.boundary = artist.grid.emptyNeighbors(startCell);
+
+    var j = 0;
+    while (j < 128*63 + 64) {
+        artist.colorNextCell();
+        j += 1;
+    }
+    console.log('ready');
+
+    // setInterval(function() {
+    //   artist.colorNextCell();
+    // }, 1);
+
+    var i = 0;
+    var id = setInterval(function() {
+      artist.coloredCells[i].paint();
+      i += 1;
+      if ( i === artist.coloredCells.length) { clearInterval(id); }
+    }, 1);
+
+  }
+
+  $("#start").click(function() {paintDisShit();});
+
+
+});
+
+var generateGrid = function(height, width) {
+  var grid = "<table>";
+  for (var y = 0; y < height; y++) {
+    var row = "<tr>";
+    for (var x = 0; x < width; x++) {
+      row += "<td id='" + x + "-" + y + "' class='cell'></td>";
+    }
+    row += "</tr>";
+    grid += row;
+  }
+  grid += "</table>";
+  return grid;
+}
+
+var coords = function(id) {
+  var splitId = id.split(",");
+  return {
+    x: parseInt(splitId[0]),
+    y: parseInt(splitId[1])
+  };
+}
